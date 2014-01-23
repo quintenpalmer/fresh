ca67df967a6d53c33a985db347476210a06bb141
Quinten <quintenpalmer@gmail.com>
2014-01-22 22:43:34 -0500

    Fix builtins and use of builtins in lib

--- haskell/src/Runtime/Builtin.hs
+++ haskell/src/Runtime/Builtin.hs
@@ -16,73 +16,92 @@ type InfiniteOperator = [Runtime.RuntimeType] -> Runtime.RuntimeType
 
 comparison :: (Int -> Int -> Bool) -> BinaryOperator
 comparison operator (Runtime.IntType x) (Runtime.IntType y) = Runtime.BoolType $ operator x y
-comparison _ _ _ =
-    error "Invalid runtime time, two ints"
+comparison _ _ _ = error "> < = Invalid runtime time, two ints"
 
 arithmetic :: (Int -> Int -> Int) -> BinaryOperator
 arithmetic operator (Runtime.IntType x) (Runtime.IntType y) = Runtime.IntType $ operator x y
-arithmetic _ _ _ =
-    error "Invalid runtime time, two ints"
+arithmetic _ _ _ = error "+ - * Invalid runtime time, two ints"
 
 boolean :: (Bool -> Bool -> Bool) -> BinaryOperator
 boolean operator (Runtime.BoolType x) (Runtime.BoolType y) = Runtime.BoolType $ operator x y
-boolean _ _ _ =
-    error "Invalid runtime time, two bools"
+boolean _ _ _ = error "and/or invalid runtime time, two bools"
 
-apply_for_all :: [Runtime.RuntimeType] -> BinaryOperator -> Bool -> Runtime.RuntimeType
-apply_for_all [] _ _ =
-    error "uhhhhh, nice try! can't apply to no arguments"
-apply_for_all [_] _ _ =
-    error "uhhhhh, nice try! can't apply to one argument"
-apply_for_all [arg1, arg2] operator _ =
-    operator arg1 arg2
-apply_for_all (argument:arguments) function accumulative =
-    function argument $ apply_for_all arguments function accumulative
+
+acculuatively_apply :: BinaryOperator -> [Runtime.RuntimeType] -> Runtime.RuntimeType
+acculuatively_apply function arguments =
+    case arguments of
+        [] -> error "uhhhhh, nice try! can't apply to no arguments"
+        [_] -> error "uhhhhh, nice try! can't apply to one argument"
+        arguments -> acc_apply_recursive function arguments
+
+acc_apply_recursive :: BinaryOperator -> [Runtime.RuntimeType] -> Runtime.RuntimeType
+acc_apply_recursive function arguments =
+    case arguments of
+        [] -> error "uhhhhh, nice try! can't apply to no arguments"
+        [arg] -> arg
+        (argument:arguments) -> function (acc_apply_recursive function arguments) argument
+
+comparatively_apply :: BinaryOperator -> [Runtime.RuntimeType] -> Runtime.RuntimeType
+comparatively_apply function arguments =
+    case arguments of
+        [] -> error "uhhhhh, nice try! can't apply to no arguments"
+        [_] -> error "uhhhhh, nice try! can't apply to one argument"
+        arguments -> comp_apply_recursive function arguments
+
+comp_apply_recursive :: BinaryOperator -> [Runtime.RuntimeType] -> Runtime.RuntimeType
+comp_apply_recursive function arguments =
+    case arguments of
+        [] -> error "uhhhhh, nice try! can't apply to no arguments"
+        [arg1, arg2] -> function arg2 arg1
+        (arg1:(arg2:arguments)) ->
+            ander
+                (function arg2 arg1)
+                (comp_apply_recursive function $ arg1:arguments)
 
 adder :: BinaryOperator
 adder x y = arithmetic (+) x y
 
 add :: InfiniteOperator
-add arguments = apply_for_all arguments adder True
+add arguments = acculuatively_apply adder arguments
 
 subtracter :: BinaryOperator
 subtracter x y = arithmetic (-) x y
 
 minus :: InfiniteOperator
-minus arguments = apply_for_all arguments subtracter True
+minus arguments = acculuatively_apply subtracter arguments
 
 multiplier :: BinaryOperator
 multiplier x y = arithmetic (*) x y
 
 multiply :: InfiniteOperator
-multiply arguments = apply_for_all arguments multiplier True
+multiply arguments = acculuatively_apply multiplier arguments
 
 ander :: BinaryOperator
 ander x y = boolean (&&) x y
 
 and_all :: InfiniteOperator
-and_all arguments = apply_for_all arguments ander True
+and_all arguments = acculuatively_apply ander arguments
 
 orer :: BinaryOperator
 orer x y = boolean (||) x y
 
 or_all :: InfiniteOperator
-or_all arguments = apply_for_all arguments orer True
+or_all arguments = acculuatively_apply orer arguments
 
 greaterer :: BinaryOperator
 greaterer x y = comparison (>) x y
 
 greater :: InfiniteOperator
-greater arguments = apply_for_all arguments greaterer False
+greater arguments = comparatively_apply greaterer arguments
 
 lesser :: BinaryOperator
 lesser x y = comparison (<) x y
 
 less :: InfiniteOperator
-less arguments = apply_for_all arguments lesser False
+less arguments = comparatively_apply lesser arguments
 
 equaler :: BinaryOperator
 equaler x y = comparison (==) x y
 
 equal :: InfiniteOperator
-equal arguments = apply_for_all arguments equaler False
+equal arguments = comparatively_apply equaler arguments
