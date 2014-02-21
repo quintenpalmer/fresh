c07713f30318f611284f9a6e90abc6c39a228799
Quinten <quintenpalmer@gmail.com>
2014-02-21 13:35:02 -0500

    Move Chars into StringParse

--- haskell/src/Lexer/StringParse.hs
+++ haskell/src/Lexer/StringParse.hs
@@ -1,27 +1,58 @@
 module Lexer.StringParse (
     get_next_non_whitespace,
     get_next_character,
+    is_delimiter,
+    is_eof,
 ) where
 
-import qualified Lexer.Chars as Chars
 import qualified Lexer.FileLoc as FileLoc
 
 type FileLocInfo = FileLoc.FileLocInfo
+
+eof :: Char
+eof = '\0'
+
+line_breaks :: [Char]
+line_breaks = ['\n']
+
+whitespace :: [Char]
+whitespace = [' ', '\r', '\t'] ++ line_breaks
+
+delimiters :: [Char]
+delimiters = ['(', ')', '[', ']', eof] ++ whitespace
+
+is_line_break :: Char -> Bool
+is_line_break char =
+    char `elem` line_breaks
+
+is_whitespace :: Char -> Bool
+is_whitespace char =
+    char `elem` whitespace
+
+is_delimiter :: Char -> Bool
+is_delimiter char =
+    char `elem` delimiters
+
+is_eof :: String -> Bool
+is_eof string =
+    case string of
+        [] -> False
+        (head_:_) -> head_ == eof
 
 get_next_non_whitespace :: String -> FileLocInfo -> (Char, String, FileLocInfo)
 get_next_non_whitespace remaining input_info =
     let (current, post_remaining, info) = get_next_character remaining input_info
     in
-        if Chars.is_whitespace current then
+        if is_whitespace current then
             get_next_non_whitespace post_remaining info
         else
             (current, post_remaining, info)
 
 get_next_character :: String -> FileLocInfo -> (Char, String, FileLocInfo)
 get_next_character [] _ = error "Reading past end of file"
-get_next_character [current] info = (current, [Chars.eof], info)
+get_next_character [current] info = (current, [eof], info)
 get_next_character (current: rest) (FileLoc.FileLocInfo (FileLoc.FileLoc start_char start_line) (FileLoc.FileLoc end_char end_line)) =
-    if Chars.is_line_break current then
+    if is_line_break current then
         (current, rest, (FileLoc.FileLocInfo (FileLoc.FileLoc 1 start_line) (FileLoc.FileLoc 1 (end_line + 1))))
     else
         (current, rest, (FileLoc.FileLocInfo (FileLoc.FileLoc start_char start_line) (FileLoc.FileLoc (end_char + 1) end_line)))
--- haskell/src/Lexer/Tokenize.hs
+++ haskell/src/Lexer/Tokenize.hs
@@ -12,7 +12,6 @@ module Lexer.Tokenize (
 import qualified Lexer.StringParse as StringParse
 import qualified Lexer.FileLoc as FileLoc
 import qualified Lexer.Literals as Literals
-import qualified Lexer.Chars as Chars
 import qualified Lexer.Tokens as Tokens
 
 type FileLocInfo = FileLoc.FileLocInfo
@@ -25,7 +24,7 @@ make_tokens input_string =
 
 to_tokens :: String -> FileLocInfo -> [Token]
 to_tokens input_string input_file_info =
-    if Chars.is_eof input_string then
+    if StringParse.is_eof input_string then
         []
     else
         let (token, remaining, file_info) = get_token input_string input_file_info
@@ -36,7 +35,7 @@ get_token :: String -> FileLocInfo -> (Token, String, FileLocInfo)
 get_token remaining (FileLoc.FileLocInfo (FileLoc.FileLoc _ _) end_file_loc_info) =
     let (current, post_remaining, file_info) = StringParse.get_next_non_whitespace remaining (FileLoc.FileLocInfo end_file_loc_info end_file_loc_info)
     in
-        if Chars.is_delimiter current then
+        if StringParse.is_delimiter current then
             (convert_token [current] file_info, post_remaining, file_info)
         else
             build_next_token [current] post_remaining file_info
@@ -49,7 +48,7 @@ build_next_token :: String -> String -> FileLocInfo -> (Token, String, FileLocIn
 build_next_token input_token remaining input_file_info =
     let (current, post_remaining, file_info) = StringParse.get_next_character remaining input_file_info
     in
-        if Chars.is_delimiter current then
+        if StringParse.is_delimiter current then
             (convert_token input_token file_info, remaining, file_info)
         else
             build_next_token (input_token ++ [current]) post_remaining file_info
@@ -60,7 +59,7 @@ get_token_type current
     | Literals.is_close_expression current = Tokens.RParen
     | Literals.is_open_arguments current = Tokens.LBracket
     | Literals.is_close_arguments current = Tokens.RBracket
-    | Chars.is_eof current = Tokens.Eof
+    | StringParse.is_eof current = Tokens.Eof
     | Literals.is_int_literal current = Tokens.IntLiteral
     | Literals.is_bool_literal current   = Tokens.BoolLiteral
     | Literals.is_null_literal current = Tokens.NullLiteral
